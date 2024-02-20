aaa877846d95c550c7acbe1a43c7dadc142aa230
J Vance <justinleevance94@gmail.com>
2024-02-20 10:52:32 -0500

    tricky tricky

--- server/app.py
+++ server/app.py
@@ -6,3 +6,35 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def home():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def show_param(parameter):
+    print(f'{parameter}')
+
+    return f'{parameter}'
+
+@app.route('/count/<int:parameter>')
+def show_count(parameter):
+    count = []
+    for i in range(0, parameter):
+        count.append(f'{str(i)}\n')
+    display = ''.join(count)
+    return display
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def show_math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == '%':
+        return str(num1 % num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    
